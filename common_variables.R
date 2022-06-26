5e306e0ed6fc5fdd3416b7b8512c54d4d4d8a7fb
Justin Gregoire <jtgregoire93@gmail.com>
2022-06-26 15:48:26 -0400

    Initial commit

---
+++ common_variables.R
@@ -0,0 +1,16 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "**Data science professional with 6 years of experience empowering organizations with data and insights.** My professional expertise covers multiple areas: defining metrics and extracting insights with imperfect data; analyzing A/B experiments and using inferential statistics to deduce product impact and opportunity; and building data pipelines to support business intelligence.
+
+I've employed my skillset across multiple cross-functional teams within a variety of industries - including ed tech, online mobile gaming, B2B cloud products, and health care.
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "SQL",                5,
+  "Python",             3
+)
