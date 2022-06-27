8f9782b8b33619984919a76966af03087dfd4d21
Justin Gregoire <jtgregoire93@gmail.com>
2022-06-27 10:39:40 -0400

    fit to one page

--- common_variables.R
+++ common_variables.R
@@ -1,7 +1,7 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "**Data science professional with 6 years of experience empowering organizations with data and insights.** My professional expertise covers multiple areas: defining metrics and extracting insights with imperfect data; analyzing A/B experiments and using inferential statistics to deduce product impact and opportunity; and building data pipelines to support business intelligence.
+intro_text <- "**Data science professional with 6 years of experience empowering organizations with data and insights.** My expertise covers multiple areas: defining metrics and extracting insights with imperfect data; analyzing A/B experiments and using inferential statistics to deduce product impact and opportunity; and building data pipelines to support business intelligence.
 
 I've employed my skillset across multiple cross-functional teams within a variety of industries - including ed tech, online mobile gaming, B2B cloud products, and health care.
 "
