7f92a886670dc47c9657631bdfe527f370960b4b
Justin Gregoire <jtgregoire93@gmail.com>
2022-06-29 12:12:23 -0400

    Update render.R
    update render to be based on parameters. Anyone using this repo should be able to declare variables for their sheet, link to their github hosted page, and the location of the pdf version on github. This is based on assumption that github pages has been set up with a <username>.github.io repo

--- render.R
+++ render.R
@@ -1,22 +1,31 @@
 # This script builds both the HTML and PDF versions of your CV
 
-library(tidyverse)
-readr::write_rds(cv_data, 'cached_positions.rds')
-cache_data <- FALSE
-
+# declare param inputs to render
+sheet_ss_id <-  "1JLnOdTkLmNy7c08_61NGYS2AlOt5fgqfPGmnKRW0pdM"
+online_link <- "https://jtgregoire93.github.io/resume/"
+pdf_location <- "https://github.com/jtgregoire93/resume/raw/main/jgregoire_resume.pdf"
 # Knit the HTML version
 rmarkdown::render("resume.rmd",
-                  params = list(pdf_mode = FALSE, cache_data = cache_data),
+                  params = list(pdf_export = FALSE, 
+                                sheet_ss_id = sheet_ss_id, 
+                                online_link = online_link, 
+                                pdf_location = pdf_location
+                                ),
                   output_file = "index.html")
 
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 rmarkdown::render("resume.rmd",
-                  params = list(pdf_mode = TRUE, cache_data = cache_data),
+                  params = list(pdf_export = TRUE, 
+                                sheet_ss_id = sheet_ss_id, 
+                                online_link = online_link, 
+                                pdf_location = pdf_location
+                  ),
                   output_file = tmp_html_cv_loc)
 
 # Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
                        output = "resume.pdf") 
 pdftools::pdf_compress("resume.pdf", output = "jgregoire_resume.pdf")
+
