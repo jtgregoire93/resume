b7ec8c6a94f5b37cc238642049ec2674950f6148
Justin Gregoire <jtgregoire93@gmail.com>
2023-03-01 13:14:20 -0800

    update link

--- render.R
+++ render.R
@@ -7,7 +7,7 @@ library(pdftools)
 # declare param inputs to render
 sheet_ss_id <- "1SqkIDpyVfurLA0DXRgcsZ9aba_Bk9bvak0sGRCLsVX0"
 online_link <- "https://jtgregoire93.github.io/resume/"
-pdf_location <- "https://github.com/jtgregoire93/resume/raw/main/resume.pdf"
+pdf_location <- "https://github.com/jtgregoire93/resume/raw/main/jgregoire_resume.pdf"
 # Knit the HTML version
 rmarkdown::render("resume.rmd", params = list(pdf_export = FALSE,
   sheet_ss_id = sheet_ss_id, online_link = online_link, pdf_location = pdf_location),
