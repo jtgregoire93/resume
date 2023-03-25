81c47e72d00f97756b0f7fcff9d9a0db647375b3
Justin Gregoire <jtgregoire93@gmail.com>
2023-03-25 16:01:29 -0700

    fix links

--- render.R
+++ render.R
@@ -6,7 +6,7 @@ library(pagedown)
 library(pdftools)
 # declare param inputs to render
 sheet_ss_id <- "1SqkIDpyVfurLA0DXRgcsZ9aba_Bk9bvak0sGRCLsVX0"
-online_link <- "https://jtgregoire93.github.io/resume/"
+online_link <- "https://justingregoire.me/resume"
 pdf_location <- "https://github.com/jtgregoire93/resume/raw/main/jgregoire_resume.pdf"
 # Knit the HTML version
 rmarkdown::render("resume.rmd", params = list(pdf_export = FALSE,
@@ -15,7 +15,7 @@ rmarkdown::render("resume.rmd", params = list(pdf_export = FALSE,
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-rmarkdown::render("resume.rmd", params = list(pdf_export = TRUE,
+rmarkdown::render("resume.Rmd", params = list(pdf_export = TRUE,
   sheet_ss_id = sheet_ss_id, online_link = online_link, pdf_location = pdf_location),
   output_file = tmp_html_cv_loc)
 
